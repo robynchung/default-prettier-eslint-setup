351af0d2a40ca14357ab1299dff8adaec67739a5
robynchung <rjungdesign@gmail.com>
2020-12-28 16:11:33 -0500

    Create .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  printWidth: 150,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+};
